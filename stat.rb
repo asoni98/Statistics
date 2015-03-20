401573b83f6ab583ee93a14dc32c1f64a66af285
asoni98 <as47359@students.westport.k12.ct.us>
2015-03-20 11:42:37 -0400

    New File Structure

--- stat.rb
+++ stat.rb
@@ -1,12 +1,12 @@
 f = File.new("unemploymentdata.csv" , "r")
-data_string = f.read
+data_in_string_form = f.read
 f.close
 
-data_array = data_string.split("\r")
-firstrow = data_array.shift
+data_in_array_form = data_in_string_form.split("\r")
+firstrow = data_in_array_form.shift
 r = 0
 
-data_array.each do |x|
+data_in_array_form.each do |x|
   data_point = x.split(',')
 data_value = data_point[1].to_f
 r = r + data_value
@@ -14,32 +14,31 @@ end
 
 avg = r/68
 n = 0
-u = avg
+mean = avg
 
-data_array.each do |x|
+data_in_array_form.each do |x|
   data_point = x.split (',')
   data_value = data_point[1].to_f
-  num = (data_value-u)**2
+  num = (data_value-mean)**2
   n = n + num
 end
 
 stdev = (n/68)**0.5
 
 new_array =[]
-data_array.each do |x|
+data_in_array_form.each do |x|
   data_point = x.split (',')
   data_value = data_point[1].to_f
-  zscore = (data_value-u)/stdev
+  zscore = (data_value-mean)/stdev
 new_array.push zscore
 end
 
 final_array = []
 68.times do |i|
-final_array.push data_array[i].to_s + "," + new_array[i].to_s
+final_array.push data_in_array_form[i].to_s + "," + new_array[i].to_s
 end
 final_array.unshift(firstrow)
 
 f = File.new("unemploymentdata2.csv" , "w")
 f.write final_array.join ("\n")
 f.close
-
